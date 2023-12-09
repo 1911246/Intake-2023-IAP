bc2ee33b328e7a0c3b49a3edc07742dd29fd32a0
1911246 <69613976+1911246@users.noreply.github.com>
2023-12-08 17:11:52 -0800

    Add files via upload

---
+++ 2023-IAP-Intake/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,48 @@
+package frc.robot;
+
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.Flywheel;
+import frc.robot.commands.Pneumatics;
+import frc.robot.subsystems.Intake;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class RobotContainer 
+{
+  //initialize all commands, subsystems, and joysticks
+  private static Intake Intake;
+  private static Joystick Joystick;
+  private static Pneumatics Pneumatics;
+  private static Flywheel Flywheel;
+  private BeamBreak beamBreaker;
+
+  //constructor
+  public RobotContainer() 
+  {
+    beamBreaker = new BeamBreak();
+    Joystick = new Joystick(Constants.Joysticks.Zero);
+    Intake = new Intake();
+    Pneumatics = new Pneumatics(Intake);
+    Flywheel = new Flywheel(Intake,Joystick);
+    Intake.setDefaultCommand(Pneumatics);
+    Intake.setDefaultCommand(Flywheel);
+    configureButtonBindings();
+  }
+
+  //getter functions
+  public static Joystick getJoy()
+  {
+    return Joystick;
+  }
+
+  private void configureButtonBindings() 
+  {
+
+  }
+
+  public Command getAutonomousCommand() 
+  {
+    return Pneumatics;
+  }
+}
